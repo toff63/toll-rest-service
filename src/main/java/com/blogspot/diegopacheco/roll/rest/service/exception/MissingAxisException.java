23146aaa59ea84e176e5a83ab2ff6461382b80be
Christophe Marchal <toff63@gmail.com>
2010-12-11 19:42:59 -0200

    Adding message for exception

--- src/main/java/com/blogspot/diegopacheco/roll/rest/service/exception/MissingAxisException.java
+++ src/main/java/com/blogspot/diegopacheco/roll/rest/service/exception/MissingAxisException.java
@@ -1,8 +1,12 @@
 package com.blogspot.diegopacheco.roll.rest.service.exception;
 
 public class MissingAxisException extends RuntimeException{
+
   public static final long serialVersionUID = -1;
 
+  public MissingAxisException(){
+    super("You must specify the number of axis in case of a truck.");
+  }
 
 }
 
